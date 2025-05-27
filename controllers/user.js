4300c318d6856a0e1c404f0159bbd75d1de41032
jairo-sousa <jairo.sousa.rufino@gmail.com>
2025-05-27 12:27:49 -0300

    refactor(userontroller): use handlePromise

--- controllers/user.js
+++ controllers/user.js
@@ -1,61 +1,30 @@
 const userModel = require("../models/user");
 
+function handlePromise(promise, reply) {
+    promise
+        .then((result) => reply.status(200).json(result))
+        .catch((error) => reply.status(400).json(error.message));
+}
+
 class UserController {
     get(req, reply) {
-        const userSent = req.body;
-
-        const response = userModel.getAll();
-
-        response
-            .then((newUser) => {
-                return reply.status(200).json(newUser);
-            })
-            .catch((error) => {
-                return reply.status(400).json(error.message);
-            });
+        handlePromise(userModel.getAll(), reply);
     }
 
     post(req, reply) {
         const userSent = req.body;
-
-        const response = userModel.post(userSent);
-
-        response
-            .then((newUser) => {
-                return reply.status(200).json(newUser);
-            })
-            .catch((error) => {
-                return reply.status(400).json(error.message);
-            });
+        handlePromise(userModel.post(userSent), reply);
     }
 
     update(req, reply) {
         const { userName } = req.params;
         const userSent = req.body;
-
-        const response = userModel.update(userName, userSent);
-
-        response
-            .then((atualUser) => {
-                return reply.status(200).json(atualUser);
-            })
-            .catch((error) => {
-                return reply.status(400).json(error.message);
-            });
+        handlePromise(userModel.update(userName, userSent), reply);
     }
 
     delete(req, reply) {
         const { userName } = req.params;
-
-        const response = userModel.delete(userName);
-
-        response
-            .then((deletedUser) => {
-                return reply.status(200).json(deletedUser);
-            })
-            .catch((error) => {
-                return reply.status(400).json(error.message);
-            });
+        handlePromise(userModel.delete(userName), reply);
     }
 }
 
